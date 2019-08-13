e2d0f11c01fb0222cef28e16f0f232d90fff71c0
Stephan Jones <sjones.csuf@gmail.com>
2019-08-12 19:17:57 -0700

    Corrected DB Connection String

--- LessonInSteam/Services/DatabaseService.cs
+++ LessonInSteam/Services/DatabaseService.cs
@@ -12,8 +12,8 @@ namespace LessonInSteam.Services
 {
     public class DatabaseService
     {
-        //string connectionString = "Server=tcp:DESKTOP-57RKN8F;Initial Catalog=LessonInSteam;Persist Security Info=False;User ID=broncostooge;Password=Toyota86!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        string connectionString = "Data Source=DESKTOP-57RKN8F;Initial Catalog=LessonInSteam;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        string connectionString = "Server=tcp:steamgamereview.database.windows.net,1433;Initial Catalog=LessonInSteam;Persist Security Info=False;User ID=broncostooge;Password=Toyota86!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        //string connectionString = "Data Source=DESKTOP-57RKN8F;Initial Catalog=LessonInSteam;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         SqlConnection cnn;
         SqlDataAdapter adapter = new SqlDataAdapter();
